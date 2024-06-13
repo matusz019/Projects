01c931a824adb9ac1015cfc5e7dc371729a20300
matusz019 <65133130+matusz019@users.noreply.github.com>
2024-06-14 00:04:40 +0100

    Created Login Page

---
+++ GymApp/app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.gymapp
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var usernameInput : EditText
+    private lateinit var passwordInput : EditText
+    private lateinit var loginBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val pass = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username : $username and Password : $pass")
+        }
+
+
+    }
+
+}
